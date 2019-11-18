b6693f07693d8f8b4b68870c49b745abe40ade34
naufalprakoso <naufalprakoso24@gmail.com>
2019-11-18 17:23:41 +0700

    Add Bottom Nav

--- app/src/main/java/com/naufalprakoso/storybook/MainActivity.kt
+++ app/src/main/java/com/naufalprakoso/storybook/MainActivity.kt
@@ -2,11 +2,19 @@ package com.naufalprakoso.storybook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        meow_bottom_nav.add(MeowBottomNavigation.Model(1, R.drawable.ic_explore_black))
+        meow_bottom_nav.add(MeowBottomNavigation.Model(2, R.drawable.ic_book_black))
+        meow_bottom_nav.add(MeowBottomNavigation.Model(3, R.drawable.ic_person_black))
+
+        meow_bottom_nav.show(1, false)
     }
 }
