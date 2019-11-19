a00437175241364e6e090cc819a964db3c0248ea
naufalprakoso <naufalprakoso24@gmail.com>
2019-11-19 17:41:00 +0700

    Add Explore

--- app/src/main/java/com/naufalprakoso/storybook/ui/explore/ExploreAdapter.kt
+++ app/src/main/java/com/naufalprakoso/storybook/ui/explore/ExploreAdapter.kt
@@ -4,8 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.naufalprakoso.storybook.R
 import com.naufalprakoso.storybook.model.Story
+import kotlinx.android.synthetic.main.item_explore.view.*
 
 class ExploreAdapter(
     private val callback: (Story) -> Unit
@@ -34,7 +36,14 @@ class ExploreAdapter(
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         fun bindItem(story: Story, callback: (Story) -> Unit) {
+            Glide.with(itemView.context).load(story.featuredImage).into(itemView.img_story)
 
+            itemView.txt_title.text = story.title
+            itemView.txt_date.text = story.datetime.substring(0, 10)
+
+            itemView.setOnClickListener {
+                callback(story)
+            }
         }
     }
 
--- app/src/main/java/com/naufalprakoso/storybook/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/naufalprakoso/storybook/ui/explore/ExploreFragment.kt
@@ -1,19 +1,24 @@
 package com.naufalprakoso.storybook.ui.explore
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.naufalprakoso.storybook.R
+import com.naufalprakoso.storybook.data.Const
 import com.naufalprakoso.storybook.model.Story
+import com.naufalprakoso.storybook.ui.story.detail.DetailStoryActivity
 import kotlinx.android.synthetic.main.fragment_explore.view.*
 
 class ExploreFragment : Fragment() {
 
     private lateinit var adapter: ExploreAdapter
+    private val stories = arrayListOf<Story>()
 
     companion object {
         fun newInstance(): Fragment {
@@ -28,7 +33,9 @@ class ExploreFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_explore, container, false)
 
         adapter = ExploreAdapter {
-
+            val intent = Intent(context, DetailStoryActivity::class.java)
+            intent.putExtra(Const.STORY_KEY, it)
+            startActivity(intent)
         }
         view.rv_stories.setHasFixedSize(true)
         view.rv_stories.layoutManager =
@@ -38,9 +45,30 @@ class ExploreFragment : Fragment() {
         return view
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        loadData()
+    }
+
     private fun loadData() {
-        val stories = ArrayList<Story>()
-        val storage = FirebaseFirestore.getInstance().collection("")
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        FirebaseFirestore.getInstance().collection("stories")
+            .whereEqualTo("userId", userId).get()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    for (document in it.result?.documents!!) {
+                        val id = document.id
+                        val story = document.toObject(Story::class.java)
+                        story?.id = id
+                        story?.let { it1 -> stories.add(it1) }
+                    }
+
+                    adapter.setStories(stories)
+                    adapter.notifyDataSetChanged()
+                }
+            }
     }
 
 }
