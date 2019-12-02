2e0b1c2754888c8c111042f77ece762112506b53
Muh Isfhani Ghiath <misfahani3@gmail.com>
2019-12-02 09:58:03 +0700

    used type of object class instead of normal class

--- app/src/main/java/com/naufalprakoso/storybook/data/Const.kt
+++ app/src/main/java/com/naufalprakoso/storybook/data/Const.kt
@@ -1,10 +1,8 @@
 package com.naufalprakoso.storybook.data
 
-class Const {
-    companion object {
-        const val SHARED_KEY = "Storybook"
-        const val USER_UID_KEY = "UserUidKey"
-        const val STORY_KEY = "StoryKey"
-        const val FRAME_KEY = "FrameKey"
-    }
+object Const {
+    const val SHARED_KEY = "Storybook"
+    const val USER_UID_KEY = "UserUidKey"
+    const val STORY_KEY = "StoryKey"
+    const val FRAME_KEY = "FrameKey"
 }
